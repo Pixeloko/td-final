0cc4a92b402c2bc199b949a39c9ad5ec1f12f51d
Pixeloko <lola-louna@outlook.fr>
2025-11-28 22:31:34 +0100

    suite, refonctionne

--- index.php
+++ index.php
@@ -4,15 +4,14 @@ $url = $_GET['url'] ?? '';
 
 switch ($url) {
     case '':
-        require __DIR__ . '/index.php';
+        require __DIR__ . '/View/home.php';
         break;
         
     case 'login':
+
         require __DIR__ . '/login.php';
         break;
-        
 
-    // Ajouter d'autres routes...
     default:
         http_response_code(404);
         echo 'Page non trouvée';
