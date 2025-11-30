407f5399d49b4e8be4c8a5e91bc900b59ccc3472
yesil-maker <i.chouiter@ecole-ipssi.net>
2025-11-30 20:53:01 +0100

    mise a jour header

--- View/header.php
+++ View/header.php
@@ -16,6 +16,21 @@ if (empty($_SESSION['csrf_token'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="../assets/style.css" rel="stylesheet">
 </head>
 <body>
+<header>
+    <nav>
+    <a href="home.php">Accueil</a>
+
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <a href="../logout.php">Déconnexion</a>
+        <a href="admin.php">Espace Admin</a>
+        <a href="publish.php">Créer un post</a>
+    <?php else: ?>
+        <a href="login.php">Connexion</a>
+    <?php endif; ?>
+</nav>
+</header>
     
+
